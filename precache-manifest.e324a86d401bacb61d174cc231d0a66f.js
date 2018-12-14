81f5dc305f20bb0fe90a592087e79caaf80f7580
Anastasiya Tanas <Anastasiya@Anastasiyas-MacBook.local>
2018-12-13 21:38:08 -0700

    Updates

--- precache-manifest.e324a86d401bacb61d174cc231d0a66f.js
+++ precache-manifest.e324a86d401bacb61d174cc231d0a66f.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/click-game/static/js/runtime~main.451cb879.js"
   },
   {
-    "revision": "852178122e0ca467f347",
-    "url": "/click-game/static/js/main.85217812.chunk.js"
+    "revision": "c3a4218f2a221add93f3",
+    "url": "/click-game/static/js/main.c3a4218f.chunk.js"
   },
   {
     "revision": "4d4949a9a9f903a5cc4a",
     "url": "/click-game/static/js/1.4d4949a9.chunk.js"
   },
   {
-    "revision": "852178122e0ca467f347",
+    "revision": "c3a4218f2a221add93f3",
     "url": "/click-game/static/css/main.60ad9770.chunk.css"
   },
   {
-    "revision": "a6d63008e83554f8cb30c6a5a376d8c8",
+    "revision": "2c60ac9905c57dcfea4965a1751ce1a6",
     "url": "/click-game/index.html"
   }
 ];
