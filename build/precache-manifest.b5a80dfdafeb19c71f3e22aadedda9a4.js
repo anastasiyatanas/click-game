04fdfb81b4b557ae7f14404b3059cc5c3907b5bc
Anastasiya Tanas <Anastasiya@Anastasiyas-MacBook.local>
2018-12-13 19:58:08 -0700

    finished homework

---
+++ build/precache-manifest.b5a80dfdafeb19c71f3e22aadedda9a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "451cb8796073a9e8640b",
+    "url": "/click-game/static/js/runtime~main.451cb879.js"
+  },
+  {
+    "revision": "852178122e0ca467f347",
+    "url": "/click-game/static/js/main.85217812.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/click-game/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "852178122e0ca467f347",
+    "url": "/click-game/static/css/main.60ad9770.chunk.css"
+  },
+  {
+    "revision": "a6d63008e83554f8cb30c6a5a376d8c8",
+    "url": "/click-game/index.html"
+  }
+];
